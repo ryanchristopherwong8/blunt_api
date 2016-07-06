5972f83c52ffd7fe35da93e503b1d833fecfd811
Ryan Wong <ryancwong8@gmail.com>
2016-07-06 04:46:19 -0700

    adds api namespace and header version control to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
-Rails.application.routes.draw do
-  
+QuickNoteApi::Application.routes.draw do
+  # Api definition
+  # Rails will automatically map that namespace to a directory matching the name under the controllers folder
+  namespace :api, defaults: { format: :json }, 
+    constraints: { subdomain: 'api' }, path: '/' do # has to have api. in the front, which has a path option set to root in relation to subdomain
+    
+    scope module: :v1 do, # version control
+      constraints: ApiConstraints.new(version: 1, default: true) do # versioning by headers => version is set to 1 in header of request
+        # We are going to list our resources here
+    end 
+
+  end
+
 end
