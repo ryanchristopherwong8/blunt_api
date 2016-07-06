194392ef12f577a7be07435bbd4416266a8fc897
Ryan Wong <ryancwong8@gmail.com>
2016-07-06 04:12:50 -0700

    initial commit and project setup

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,4 @@
+class ApplicationController < ActionController::API
+  protect_from_forgery with: :null_session
+  include DeviseTokenAuth::Concerns::SetUserByToken
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+QuicknoteApi::Application.config.secret_token = 'ceb7f0a4745a3b8a3cbebef60c2fadcdc7900dcbf04e8173e1dfeb00e64193510b3639da408c23019d1ab2df529c692dceae9881a148cee25532daf01fec1262'
