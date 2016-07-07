f8613cdf0773cf6a80f42271988f6c367b1a0798
Ryan Wong <ryancwong8@gmail.com>
2016-07-07 03:30:45 -0700

    Adds authenticable module for managing authentication methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::API
   include ActionController::RequestForgeryProtection
   protect_from_forgery with: :null_session
   respond_to :json 
+  include Authenticable
 end
