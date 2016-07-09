429bc84e92528ac572369d88f38db43e83a0beee
Ryan Wong <ryancwong8@gmail.com>
2016-07-09 06:50:33 -0700

    Removes subdomain api for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ QuicknoteApi::Application.routes.draw do
   devise_for :users, defaults: { format: :json}
   # Api definition
   # Rails will automatically map that namespace to a directory matching the name under the controllers folder
-  namespace :api, defaults: { format: :json }, 
-    constraints: { subdomain: 'api' }, path: '/' do # has to have api. in the front, which has a path option set to root in relation to subdomain
+  namespace :api, defaults: { format: :json }, path: '/' do # has to have api. in the front, which has a path option set to root in relation to subdomain
     
     scope module: :v1, # version control
       constraints: ApiConstraints.new(version: 1, default: true) do # versioning by headers => version is set to 1 in header of request
